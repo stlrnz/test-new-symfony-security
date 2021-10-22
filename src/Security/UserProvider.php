29099a018bff259a0e65a04c0160fa9465bbb42e
stlrnz <stlrnz@users.noreply.github.com>
2021-10-22 09:58:15 +0200

    added logging

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace App\Security;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 final class UserProvider implements UserProviderInterface
 {
+    private $logger;
+
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
 
     public function refreshUser(UserInterface $user)
     {
+        $this->logger->debug('Refreshing user ' . $user->getUserIdentifier());
+
         return $user;
     }
 
@@ -24,6 +35,8 @@ final class UserProvider implements UserProviderInterface
 
     public function loadUserByUsername(string $username)
     {
+        $this->logger->debug('Loading user ' . $username);
+
         return new User($username);
     }
 
